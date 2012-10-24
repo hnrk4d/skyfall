fb40346ae524439e19997724974701cdb01e41bb
Henrik Battke <henrik.battke@nokia.com>
2012-10-24 21:29:25 +0200

    init Android part of the project

---
+++ android/src/org/ig4d/skyterm/PictureActivity.java
@@ -0,0 +1,249 @@
+package org.ig4d.skyterm;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
+import android.text.format.Time;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class PictureActivity extends Activity {
+	private static String TAG = "CAM: ";
+	List<String> mLogStrings = new ArrayList<String>(10);
+
+	private SurfaceView mPreview = null;
+	private SurfaceHolder mPreviewHolder = null;
+	private Camera mCamera = null;
+	private boolean mInPreview = false;
+	private boolean mCameraConfigured = false;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_picture);
+
+		mPreview = (SurfaceView) findViewById(R.id.camera_preview);
+		mPreviewHolder = mPreview.getHolder();
+		mPreviewHolder.addCallback(surfaceCallback);
+		mPreviewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+		mLogStrings.add(TAG + "Picture activity created.");
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		// if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+		// Camera.CameraInfo info = new Camera.CameraInfo();
+		//
+		// for (int i = 0; i < Camera.getNumberOfCameras(); i++) {
+		// Camera.getCameraInfo(i, info);
+		//
+		// if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+		// mCamera = Camera.open(i);
+		// }
+		// }
+		// }
+
+		mCamera = getCameraInstance();
+
+		if (mCamera == null) {
+			mCamera = Camera.open();
+		}
+
+		startPreview();
+		mPreview.postDelayed(mCameraTimer, 100);
+	}
+
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e) {
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
+	}
+
+	@Override
+	public void onPause() {
+		if (mInPreview) {
+			mCamera.stopPreview();
+		}
+
+		mCamera.release();
+		mCamera = null;
+		mInPreview = false;
+
+		super.onPause();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.activity_sky_term, menu);
+		return (super.onCreateOptionsMenu(menu));
+	}
+
+	private Runnable mCameraTimer = new Runnable() {
+		public void run() {
+			if (mInPreview) {
+				mCamera.takePicture(null, null, photoCallback);
+				mInPreview = false;
+			}
+		}
+	};
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return (super.onOptionsItemSelected(item));
+	}
+
+	private Camera.Size getBestPreviewSize(int width, int height,
+			Camera.Parameters parameters) {
+		Camera.Size result = null;
+
+		for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+			if (size.width <= width && size.height <= height) {
+				if (result == null) {
+					result = size;
+				} else {
+					int resultArea = result.width * result.height;
+					int newArea = size.width * size.height;
+
+					if (newArea > resultArea) {
+						result = size;
+					}
+				}
+			}
+		}
+
+		return (result);
+	}
+
+	private Camera.Size getBiggestPictureSize(Camera.Parameters parameters) {
+		Camera.Size result = null;
+
+		for (Camera.Size size : parameters.getSupportedPictureSizes()) {
+			if (result == null) {
+				result = size;
+			} else {
+				int resultArea = result.width * result.height;
+				int newArea = size.width * size.height;
+
+				if (newArea > resultArea) {
+					result = size;
+				}
+			}
+		}
+
+		return (result);
+	}
+
+	private void initPreview(int width, int height) {
+		if (mCamera != null && mPreviewHolder.getSurface() != null) {
+			try {
+				mCamera.setPreviewDisplay(mPreviewHolder);
+			} catch (Throwable t) {
+				mLogStrings.add(TAG + "Exception in setPreviewDisplay():"
+						+ t.getMessage());
+			}
+
+			if (!mCameraConfigured) {
+				Camera.Parameters parameters = mCamera.getParameters();
+				Camera.Size size = getBestPreviewSize(width, height, parameters);
+				Camera.Size pictureSize = getBiggestPictureSize(parameters);
+
+				if (size != null && pictureSize != null) {
+					parameters.setPreviewSize(size.width, size.height);
+					parameters.setPictureSize(pictureSize.width,
+							pictureSize.height);
+					parameters.setPictureFormat(ImageFormat.JPEG);
+					mCamera.setParameters(parameters);
+					mCameraConfigured = true;
+				}
+			}
+		}
+	}
+
+	private void startPreview() {
+		if (mCameraConfigured && mCamera != null) {
+			mCamera.startPreview();
+			mInPreview = true;
+		}
+	}
+
+	SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+		public void surfaceCreated(SurfaceHolder holder) {
+			// no-op -- wait until surfaceChanged()
+		}
+
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
+			initPreview(width, height);
+			startPreview();
+		}
+
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			// no-op
+		}
+	};
+
+	Camera.PictureCallback photoCallback = new Camera.PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			saveImg(data);
+			camera.startPreview();
+			mInPreview = true;
+
+			// back to main view
+			Intent intObj = new Intent(PictureActivity.this, SkyTermActivity.class);
+			String str=new String();
+			Time time = new Time();
+			time.setToNow();
+			for(int i=0; i<mLogStrings.size(); ++i) {
+				str+=time.format("%k:%M:%S") + ":" + mLogStrings.get(i)+"\n";
+			}
+			intObj.putExtra("camlog", str);
+			setResult(RESULT_OK, intObj);
+
+			finish();
+		}
+	};
+
+	protected void saveImg(byte[]... jpeg) {
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		File photo = new File(Environment.getExternalStorageDirectory()
+				+ File.separator + "Pictures", "SKY_" + timeStamp + ".jpg");
+
+		if (photo.exists()) {
+			photo.delete();
+		}
+
+		try {
+			FileOutputStream fos = new FileOutputStream(photo.getPath());
+
+			fos.write(jpeg[0]);
+			fos.close();
+			mLogStrings.add(TAG + "saved picture to " + photo.getName());
+		} catch (java.io.IOException e) {
+			mLogStrings.add(TAG + "Exception in photoCallback:"
+					+ e.getMessage());
+		}
+	}
+}
---
+++ android/src/org/ig4d/skyterm/SkyTermActivity.java
@@ -0,0 +1,493 @@
+package org.ig4d.skyterm;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+//import android.content.Intent;
+import android.text.format.Time;
+import android.view.Menu;
+import android.widget.EditText;
+
+public class SkyTermActivity extends Activity {
+
+	//http://saigeethamn.blogspot.de/2009/09/android-developer-tutorial-for_04.html
+    //http://developer.android.com/reference/android/app/Service.html
+
+	EditText mTextbox;
+	BluetoothAdapter mBluetoothAdapter;
+	BluetoothSocket mSocket;
+	BluetoothDevice mDevice;
+	OutputStream mOutputStream;
+	InputStream mInputStream;
+	Thread mWorkerThread;
+	byte[] mReadBuffer;
+	int mReadBufferPosition;
+	int mCounter;
+	boolean mCreated = false;
+	volatile boolean mStopWorker;
+	Time mTime = new Time(Time.getCurrentTimezone());
+	List<String> mLogStrings = new ArrayList<String>(10);
+	private static final int CLOSE_TIMEOUT = 1000;
+
+	private String mTitle;
+	private String mTitleString=new String();
+
+	private int REQUEST_CODE = 1;
+
+	private static final String ERR = "ERR: ";
+	private static final String INFO = "INFO: ";
+
+	private static final String DEVICE = "japanboxz";
+
+	private static enum State {
+		STATE_START_UP, STATE_CONNECT, STATE_COMMUNICATE, STATE_CLOSE
+	};
+
+	private State mState = State.STATE_START_UP;
+
+	// Positioning
+	// private LocationProvider mLocationProvider =
+	// LocationManager.GPS_PROVIDER;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sky_term);
+		mTitle = getResources().getString(R.string.title_activity_sky_term);
+		
+		if (!mCreated) {
+			openLogFile();
+
+			mTextbox = (EditText) findViewById(R.id.editText);
+
+			mTextbox.postDelayed(mBTTimer, 500);
+
+			// mLocationManager.requestLocationUpdates(mLocationProvider, 0, 0,
+			// mLocationListener);
+
+			mTextbox.postDelayed(mTakePicture, 2000);
+			mCreated = true;
+			
+			logln("Activity created.");
+		}
+		else {
+			logln("Activity exists already.");
+		}
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+			String logs = data.getStringExtra("camlog");
+			if (logs != null) {
+				log(logs);
+			}
+		}
+	}
+
+	private Runnable mBTTimer = new Runnable() {
+		public void run() {
+			switch (mState) {
+			// ------------------------------------------------------------------------------------------------
+			case STATE_START_UP: {
+				mState = State.STATE_CONNECT;
+				mTextbox.postDelayed(mBTTimer, 100); // start connect
+			}
+				break;
+			// ------------------------------------------------------------------------------------------------
+			case STATE_CONNECT: {
+				if (mBluetoothAdapter != null
+						&& (mBluetoothAdapter.getState() == BluetoothAdapter.STATE_TURNING_OFF)) {
+					// wait until device is turned off
+					mTextbox.postDelayed(mBTTimer, 1000); // try again after x
+															// sec
+				}
+				try {
+					if (findBT() && openBT()) {
+						mState = State.STATE_COMMUNICATE;
+						beginListenForData();
+						mTextbox.postDelayed(mBTTimer, 1000); // start
+																// communication
+					} else {
+						logln(ERR + "find/open BT failed");
+						closeBT();
+						mState = State.STATE_CONNECT;
+						mTextbox.postDelayed(mBTTimer, CLOSE_TIMEOUT); // try
+																		// again
+																		// after
+																		// x sec
+					}
+				} catch (IOException ex) {
+					logln(ERR + ex.getMessage());
+					closeBT();
+					mState = State.STATE_CONNECT;
+					mTextbox.postDelayed(mBTTimer, CLOSE_TIMEOUT); // try again
+																	// after x
+																	// sec
+				}
+			}
+				break;
+			// ------------------------------------------------------------------------------------------------
+			case STATE_COMMUNICATE: {
+				if (mStopWorker == true) {
+					// something failed in the worker thread, we try to
+					// reestablish the communication
+					logln(ERR + "failed to listen for data");
+					closeBT();
+					mState = State.STATE_CONNECT;
+					mTextbox.postDelayed(mBTTimer, CLOSE_TIMEOUT); // try again
+																	// after x
+																	// sec
+				} else {
+					try {
+						sendCmd("ping");
+					} catch (IOException ex) {
+						logln(ERR + "COMM " + ex.getMessage());
+						closeBT();
+						mState = State.STATE_CONNECT;
+					}
+					mTextbox.postDelayed(mBTTimer, 1000);
+				}
+			}
+				break;
+			// ------------------------------------------------------------------------------------------------
+			case STATE_CLOSE: {
+				closeBT();
+				mState = State.STATE_CONNECT;
+				mTextbox.postDelayed(mBTTimer, CLOSE_TIMEOUT); // restart after
+																// 10 sec
+			}
+				break;
+			}
+			// log everything that has is stored for logging
+			for (String temp : mLogStrings) {
+				mTextbox.append(temp);
+			}
+			mLogStrings.clear();
+		}
+	};
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_sky_term, menu);
+		return true;
+	}
+
+	boolean findBT() {
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			logln(ERR + "No bluetooth adapter available");
+			return false;
+		}
+
+		if (!mBluetoothAdapter.isEnabled()) {
+			mBluetoothAdapter.enable();
+			try {
+				Thread.sleep(100);
+			} catch (Throwable e) {
+			}
+			// Intent enableBluetooth = new
+			// Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			// startActivityForResult(enableBluetooth, 0);
+		}
+
+		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter
+				.getBondedDevices();
+		if (pairedDevices.size() > 0) {
+			for (BluetoothDevice device : pairedDevices) {
+				if (device.getName().equals(DEVICE)) {
+					mDevice = device;
+					logln(INFO + "Bluetooth device '" + DEVICE
+							+ "' is a paired device");
+					return true;
+				}
+			}
+		}
+		logln(ERR + "Bluetooth device '" + DEVICE + "' not found");
+		return false;
+	}
+
+	boolean openBT() throws IOException {
+		UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); // Standard
+																				// SerialPortService
+																				// ID
+		if (mDevice != null) {
+			mSocket = mDevice.createRfcommSocketToServiceRecord(uuid);
+			mSocket.connect();
+			mOutputStream = mSocket.getOutputStream();
+			mInputStream = mSocket.getInputStream();
+
+			beginListenForData();
+
+			logln(INFO + "Bluetooth opened");
+			return true;
+		} else {
+			logln(ERR + "Bluetooth device '" + DEVICE + "' unavailable");
+		}
+		return false;
+	}
+
+	void beginListenForData() {
+		final Handler handler = new Handler();
+		final byte delimiter = '\n';
+
+		mStopWorker = false;
+		mReadBufferPosition = 0;
+		mReadBuffer = new byte[1024];
+		mWorkerThread = new Thread(new Runnable() {
+			public synchronized boolean readData() {
+				boolean result = true;
+				try {
+					int bytesAvailable = mInputStream.available();
+					if (bytesAvailable > 0) {
+						byte[] packetBytes = new byte[bytesAvailable];
+						mInputStream.read(packetBytes);
+						for (int i = 0; i < bytesAvailable; i++) {
+							byte b = packetBytes[i];
+							if (b == delimiter) {
+								byte[] encodedBytes = new byte[mReadBufferPosition];
+								System.arraycopy(mReadBuffer, 0, encodedBytes,
+										0, encodedBytes.length);
+								String data = new String(encodedBytes,
+										"US-ASCII");
+								final String str = data.replaceAll(
+										"[^A-Za-z0-9: ]", "");
+								mReadBufferPosition = 0;
+
+								handler.post(new Runnable() {
+									public void run() {
+										// process data
+										process(str);
+									}
+								});
+							} else {
+								mReadBuffer[mReadBufferPosition++] = b;
+							}
+						}
+					}
+				} catch (IOException ex) {
+					logln(ERR + "WORK " + ex.getMessage());
+					result = false;
+				}
+				return result;
+			}
+
+			public void run() {
+				while (!Thread.currentThread().isInterrupted() && !mStopWorker) {
+					int i;
+					boolean res = false;
+					for (i = 0; i < 3 && !res; ++i) {
+						res = readData();
+					}
+					if (!res) {
+						// failed too often
+						mStopWorker = true;
+					}
+					try {
+						Thread.sleep(10);
+					} catch (Throwable e) {
+					}
+				}
+			}
+		});
+
+		mWorkerThread.start();
+	}
+
+	private void process(String cmd) {
+		if (cmd.toLowerCase().startsWith("log:")) {
+			logln(INFO + cmd);
+		} else if (cmd.compareToIgnoreCase("ack") == 0) {
+			logTitle("-");
+		}
+	}
+
+	synchronized void sendCmd(String msg) throws IOException {
+		byte[] bytes = (msg).getBytes("US-ASCII");
+		byte xor = 0;
+		for (int i = 0; i < bytes.length; ++i) {
+			xor ^= bytes[i];
+			mOutputStream.write(bytes[i]);
+			try {
+				Thread.sleep(10);
+			} catch (Throwable e) {
+			}
+		}
+		mOutputStream.write(xor);
+		try {
+			Thread.sleep(10);
+		} catch (Throwable e) {
+		}
+		mOutputStream.write('\n');
+		logTitle("+");
+	}
+
+	void closeBT() {
+		mStopWorker = true;
+		try {
+			Thread.sleep(100);
+		} catch (Throwable e) {
+		}
+		if (mOutputStream != null) {
+			try {
+				mOutputStream.close();
+				mOutputStream = null;
+			} catch (Throwable ex) {
+				logln(ERR + ex.getMessage());
+			}
+		}
+		if (mInputStream != null) {
+			try {
+				mInputStream.close();
+				mInputStream = null;
+			} catch (Throwable ex) {
+				logln(ERR + ex.getMessage());
+			}
+		}
+		if (mSocket != null) {
+			try {
+				mSocket.close();
+				mSocket = null;
+			} catch (Throwable ex) {
+				logln(ERR + ex.getMessage());
+			}
+		}
+		mDevice = null;
+		if (mBluetoothAdapter != null) {
+			mBluetoothAdapter.disable();
+		}
+		// mBluetoothAdapter=null;
+		System.gc();
+		logln(INFO + "Bluetooth closing ...");
+	}
+
+	void logln(String text) {
+		mTime.setToNow();
+		String str = mTime.format("%k:%M:%S") + ":" + text + "\n";
+		mLogStrings.add(str);
+		writeToFile(str);
+	}
+
+	void log(String text) {
+		mLogStrings.add(text);
+		writeToFile(text);
+	}
+
+	FileOutputStream mF;
+	PrintWriter mPw;
+
+	private boolean openLogFile() {
+		File root;
+		boolean externalStorageAvailable = false;
+		boolean externalStorageWriteable = false;
+		String state = Environment.getExternalStorageState();
+
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			// We can read and write the media
+			externalStorageAvailable = externalStorageWriteable = true;
+		} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+			// We can only read the media
+			externalStorageAvailable = true;
+			externalStorageWriteable = false;
+		} else {
+			// Something else is wrong. It may be one of many other states, but
+			// all we need
+			// to know is we can neither read nor write
+			externalStorageAvailable = externalStorageWriteable = false;
+		}
+
+		if (externalStorageAvailable && externalStorageWriteable) {
+			root = android.os.Environment.getExternalStorageDirectory();
+		} else {
+			root = android.os.Environment.getDataDirectory();
+		}
+
+		File dir = new File(root.getAbsolutePath() + "/download");
+		dir.mkdirs();
+
+		File ofile = new File(dir, "skyterm_log.txt");
+		if (ofile.exists()) {
+			File nfile = new File(dir, "skyterm_log_old.txt");
+			ofile.renameTo(nfile);
+		}
+		File file = new File(dir, "skyterm_log.txt");
+
+		try {
+			mF = new FileOutputStream(file);
+			mPw = new PrintWriter(mF);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			mPw = null;
+			mF = null;
+			return false;
+		}
+		return true;
+	}
+
+	// private void closeLogFile() {
+	// try {
+	// if(mPw != null) {
+	// mPw.flush();
+	// mPw.close();
+	// }
+	// if(mF != null) {
+	// mF.close();
+	// }
+	// } catch (FileNotFoundException e) {
+	// e.printStackTrace();
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	// }
+
+	private void writeToFile(String str) {
+		if (mPw != null) {
+			mPw.print(str);
+			mPw.flush();
+		}
+	}
+
+	private Runnable mTakePicture = new Runnable() {
+		public void run() {
+			Intent intObj = new Intent(SkyTermActivity.this,
+					PictureActivity.class);
+			startActivityForResult(intObj, REQUEST_CODE);
+		}
+	};
+
+    private void logTitle(String str) {
+    	int len=20;
+    	mTitleString += str;
+    	if(mTitleString.length() > len) {
+    		mTitleString=mTitleString.substring(mTitleString.length() - len);
+    	}
+    	setTitle(mTitle+" ["+mTitleString+"]");
+    }
+}
